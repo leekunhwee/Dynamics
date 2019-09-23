ac6148cde6d81631b4562117e2896507dac37b99
李健辉 <leekunhwee@gmail.com>
2019-09-22 23:32:03 -0700

    Update PlotLobe.m

--- Cutting/Stability/FrequencyDomain/Turning/PlotLobe.m
+++ Cutting/Stability/FrequencyDomain/Turning/PlotLobe.m
@@ -23,7 +23,7 @@ FRF_real = FRF_real(index);
 FRF_imag = FRF_imag(index);
 w = w(index);
 
-% Separate the two modals
+% Separate the two modals 
 cou=0;
 for cnt=1:length(index)-1
     if index(cnt+1)-index(cnt)==1
@@ -40,6 +40,7 @@ FRF_real2=FRF_real(cou+1:end);
 FRF_imag2=FRF_imag(cou+1:end);
 w1=w(1:cou);
 w2=w(cou+1:end);
+
 % Define force parameters
 Kf=1000e6;          % N/m^2
  
@@ -96,3 +97,52 @@ grid on
 
 annotation('textarrow',[.597,.57],[.75,.68],'String','150Hz','FontName', 'Times New Roman'); %[x1,x2],[y1,y2]
 annotation('textarrow',[.76,.68],[.75,.68],'String','250Hz','FontName', 'Times New Roman'); %[x1,x2],[y1,y2]
+
+%% Put modes together
+clear
+
+load FRF
+
+%% Determine valid chatter frequency range
+FRF_real=real(FRF);
+FRF_imag=imag(FRF);
+index = find(FRF_real < 0);
+FRF_real = FRF_real(index);
+FRF_imag = FRF_imag(index);
+w = w(index);
+
+% Define force parameters
+Kf=1000e6;          % N/m^2
+
+%% Plot Lobe
+% Calculate blim
+blim = -1./(2*Kf*FRF_real);  % m
+blim = blim*1e3;        % convert to mm
+
+% Calculate epsilon
+for cnt = 1:length(FRF_imag)
+    if FRF_imag(cnt)<0
+        epsilon(cnt)=2*pi-2*atan(abs(FRF_real(cnt)/FRF_imag(cnt)));
+    else
+        epsilon(cnt)=pi-2*atan(abs(FRF_imag(cnt)/FRF_real(cnt)));
+    end
+end
+
+
+% Calculate spindle speeds for N = 0 to 2
+omega0 = w./(0*2*pi + epsilon);   % rps
+omega1 = w./(1*2*pi + epsilon);
+omega2 = w./(2*2*pi + epsilon);   % rps
+omega3 = w./(3*2*pi + epsilon);
+
+figure(4)
+plot(omega0*60, blim, 'g', omega1*60, blim, 'g', omega2*60, blim, 'g', omega3*60, blim, 'g','linewidth',2)
+axis([2000 15000 0 60])
+set(gca,'FontSize', 14)
+xlabel('\fontsize{10}\fontname{Times New Roman} \Omega / rpm')
+ylabel('\fontsize{10}\fontname{Times New Roman}\itb_{lim} \rm/ mm')
+set(gca,'FontSize', 10 ,'FontName', 'Times New Roman')
+set(gcf,'unit','centimeters','position',[15 15 13.53 9.03],'color','white');
+title('\fontsize{10}Lobe Diagram')
+hold on
+grid on
