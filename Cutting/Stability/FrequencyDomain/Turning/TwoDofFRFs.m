bebdb372861504c4cc233ec5fee103fe854a897d
李健辉 <leekunhwee@gmail.com>
2019-09-23 11:46:43 -0700

    Modify

--- Cutting/Stability/FrequencyDomain/Turning/TwoDofFRFs.m
+++ Cutting/Stability/FrequencyDomain/Turning/TwoDofFRFs.m
@@ -13,38 +13,54 @@
 clc
 clear
 close all
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Reference£º
+%Altintas, Y. (2012). Manufacturing Automation: 
+%Metal Cutting Mechanics, Machine Tool Vibrations, and CNC Design. 
+%Applied Mechanics Reviews (Vol. 54). https://doi.org/10.1115/1.1399383
+%P131
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Define modal parmeters
-k = [2.26e8 2.13e8];               % N/m
-zeta = [0.012 0.010];
-wn = [250 150]*2*pi;               % rad/s
-f_start=0;
-f_end=300-0.5;
-df=0.1;
-w = (f_start:df:f_end)*2*pi;   % frequency, rad/s
-f=w/2/pi;
+k    = [2.26e8 2.13e8];               % Modal stiffness N/m
+zeta = [0.012  0.010 ];               % Modal damping ratio
+wn   = [250    150   ]*2*pi;          % Natural frequency rad/s
 
-%% FRF Synthesis
-FRF1 = cos(30/180*pi)^2*(wn(1)^2/k(1))./(wn(1)^2 - w.^2 + 1i*2*zeta(1)*wn(1).*w);
+f_start= 0;
+f_end  = 300-0.5;
+df     = 0.1;
+w      = (f_start:df:f_end)*2*pi;   % frequency, rad/s
+f      = w/2/pi;
+
+%% FRF Synthesis (To one direction)
+FRF1 = cos( 30/180*pi)^2*(wn(1)^2/k(1))./(wn(1)^2 - w.^2 + 1i*2*zeta(1)*wn(1).*w);
 FRF2 = cos(-45/180*pi)^2*(wn(2)^2/k(2))./(wn(2)^2 - w.^2 + 1i*2*zeta(2)*wn(2).*w);
-FRF = FRF1 + FRF2;
+FRF  = FRF1 + FRF2;
+
 % for cnt = 2:length(k)
 %     FRF = FRF + (wn(cnt)^2/k(cnt))./(wn(cnt)^2 - w.^2 + 1i*2*zeta(cnt)*wn(cnt).*w);
 % end
-FRFMag=abs(FRF);
 
-%% Plot FRF Magnitude
+%% Plot FRF Magnitude and Phase
 figure(1)
-plot(f,FRFMag,'b-','linewidth',2)
+subplot(211)
+plot(f,abs(FRF),'b-','linewidth',2)
 hold on
 xlim([0,300])
 grid on  
-legend('\fontsize{10}\fontname{Times New Roman}\itFRF','location','northwest')
+% legend('\fontsize{10}\fontname{Times New Roman}\itFRF','location','northwest')
+set(gca,'FontSize', 10 ,'FontName', 'Times New Roman')
+ylabel('\fontsize{10}\fontname{Times New Roman}\it FRF\rm/ m.N^{-1}')
+subplot(212)
+plot(f,angle(FRF)*180/pi,'b-','linewidth',2)
+xlim([0,300])
+ylim([-180,0])
+set(gca,'ytick',[-180 -140 -100 -60 -20 0])
+grid on 
 set(gca,'FontSize', 10 ,'FontName', 'Times New Roman')
 set(gcf,'unit','centimeters','position',[0 10 13.53 9.03],'color','white');
 title('\fontsize{10}FRF')
 xlabel('\fontsize{10}\fontname{Times New Roman}\it f\rm/ Hz')
-ylabel('\fontsize{10}\fontname{Times New Roman}\it FRF\rm/ m.N^{-1}')
+ylabel('\fontsize{10}\fontname{Times New Roman} degree/ ^\circ')
 hold on
 
 %% Plot FRF Real-Image Parts
@@ -56,6 +72,7 @@ grid on
 title('\fontsize{10}FRF')
 xlim([0,300])
 set(gca,'FontSize', 10 ,'FontName', 'Times New Roman')
+ylabel('\fontsize{10}\fontname{Times New Roman}\it FRF\rm/ m.N^{-1}')
 subplot(212)
 plot(f,imag(FRF),'b-','linewidth',2)
 xlim([0,300])
