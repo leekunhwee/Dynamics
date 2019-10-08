7e8be56b078f6e8f003a2dd5b7b2924e05bc3ba5
李健辉 <leekunhwee@gmail.com>
2019-10-08 13:16:13 -0700

    Debug

--- Cutting/Stability/TimeDomain/SemiDis/Turning/OneDOFTurningUpdate.m
+++ Cutting/Stability/TimeDomain/SemiDis/Turning/OneDOFTurningUpdate.m
@@ -14,7 +14,7 @@
 % Reduce the dimension from 2(m+1) to m+2
 clc
 clear
-% close all
+close all
 
 %% Dynamic Parameters
 % One DOF
@@ -26,9 +26,8 @@ wn = 250*2*pi;              % Natural Frequency rad/sec
 K_f = 1e9;                  % Cutting Constant N/m2
 
 %% Simulation Parameters
-k=80;               % number of discretization interval over one period T
-m=k;                % since time delay = time period
-% deltaT = 0.1e-3;    % sec
+k = 40;               % number of discretization interval over one period T
+m = k;                % since time delay = time period
 depth_st = 0e-3;    % Starting depth of cut(m)
 depth_fi = 60e-3;   % Final depth of cut(m)
 speed_st = 2e3;     % Starting spindle speed(rpm)
@@ -45,15 +44,13 @@ for s = 1:speed_step+1
     % Spindle period
     T = 60/n;
     % Discrete time number
-%     m = round(T/deltaT);
-    dt = T/m;
+    dt = T/k;
     % Cutting depth loop
-%     da = 0.2e-3; % Axis depth step(m)
     for d = 1:depth_step+1
         % Build the L and R matrix
         a = depth_st+(d-1)*(depth_fi-depth_st)/depth_step;
         L = [0, 1; -(wn^2)*(1+(K_f*a/k_y)), -2*zeta*wn];
-        R = [0, 0; (wn^2)*K_f*a/k_y, 0];
+        R = [0, 0; (wn^2)*K_f*a/k_y       , 0         ];
         % Initial the B1 and B2 matrix, Attention! Column and row
         B1 = zeros(m+2,m+2);
         B2 = zeros(m+2,m+2);
@@ -63,9 +60,12 @@ for s = 1:speed_step+1
         B1(3,1) = 1;
         % Define the first two Column and Row of B1
         B1(1:2,1:2) = expm(L*dt);   
-        % Build the complete matrix of B2 
-        B2((1:2),(end-1:end))   = 0.5*(expm(L*dt)-I)/L*R;
-        B2((1:2),(end-3:end-2)) = 0.5*(expm(L*dt)-I)/L*R;
+        % Build the complete matrix of B2
+        BEnd1 = 0.5*(expm(L*dt)-I)/L*R;
+        BEnd2 = 0.5*(expm(L*dt)-I)/L*R;
+        B2((1:2),(end))   = BEnd1(1:2,1);
+        B2((1:2),(end-1)) = BEnd2(1:2,1);
+
         % Build the B matrix
         B = B1+B2;
         Fi = eye(m+2,m+2); 
@@ -79,13 +79,10 @@ for s = 1:speed_step+1
     speed_step+1-s          % Process Display
 end
 %%
-figure 
+hold on
+% figure 
 contour(ss,dp,ei,[1, 1])
 
-% % hold on
-% figure;
-% 
-% plot(unstableN,unstableA*1000,'.');
 xlabel('Spindle speed [rev/min]');
 ylabel('a_l_i_m [mm]');
 title('Stability of a 1-DOF shaping process');
