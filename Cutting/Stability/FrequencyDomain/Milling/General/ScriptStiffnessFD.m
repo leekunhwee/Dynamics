1aad6d70b1c135e806db9c2af50e1a850ba70c88
李健辉 <leekunhwee@gmail.com>
2019-10-17 22:18:17 -0700

    modify

--- Cutting/Stability/FrequencyDomain/Milling/General/ScriptStiffnessFD.m
+++ Cutting/Stability/FrequencyDomain/Milling/General/ScriptStiffnessFD.m
@@ -7,18 +7,6 @@ flag = 2; % Stiffness type
 %% Parameters
 Parameter = struct;
 % Dynamics data
-% Parameter.Dynamic.f.x = [456.780432115313 1448.88914030656];
-% Parameter.Dynamic.zeta.x = [0.111705399393456 0.0170370095790783];
-% Parameter.Dynamic.m.x = [0.963094248615537 0.177274003430936];
-% Parameter.Dynamic.k.x = [7933097.18086825 14691778.4389479];
-% Parameter.Dynamic.shape.x = [1.01897987173574 2.37507579510009];
-% 
-% Parameter.Dynamic.f.y = [516.51818914797 1408.44814086966];
-% Parameter.Dynamic.zeta.y = [0.0245796327070992 0.0313576019295683];
-% Parameter.Dynamic.m.y = [0.89006790971493 0.158418061422864];
-% Parameter.Dynamic.k.y = [9374613.81193087 12406389.7145168];
-% Parameter.Dynamic.shape.y = [1.05995744178464 2.51245130218873];
-
 Parameter.Dynamic.f.x = [456.780432115313 1448.88914030656];
 Parameter.Dynamic.zeta.x = [0.111705399393456 0.0170370095790783];
 Parameter.Dynamic.m.x = [0.963094248615537 0.177274003430936];
@@ -30,7 +18,6 @@ Parameter.Dynamic.zeta.y = [0.0245796327070992 0.0313576019295683];
 Parameter.Dynamic.m.y = [0.89006790971493 0.158418061422864];
 Parameter.Dynamic.k.y = [9374613.81193087 12406389.7145168];
 Parameter.Dynamic.shape.y = [1.05995744178464 2.51245130218873];
-
 % Simulation parameter
 Parameter.Simulation.f_start = 0;       % Hz
 Parameter.Simulation.f_end = 8000;  % Hz
